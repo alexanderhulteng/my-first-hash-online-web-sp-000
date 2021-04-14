e290aee080627430ef91a56772417adbc3bbba4d
Alexander Hulteng <alexanderhulteng@gmail.com>
2021-04-14 19:03:46 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -42,7 +42,10 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
+  shipping_manifest["gun powder"] = 4
+  shipping_manifest["muskets"] = 2
+
+  shipping_manifest
 
 
 
